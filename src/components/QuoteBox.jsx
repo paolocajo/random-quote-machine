65abe712537a4c5e31ffdbafbdf0ae4580fede28
Paolo Martín Cajo Flores <paolocajo09@gmail.com>
2023-08-31 18:58:46 -0500

    Consuming quoteable API and adding loader

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,10 +1,23 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import twitterSVG from "/twitter.svg";
 import VanillaTilt from "vanilla-tilt";
+import { useFetch } from "../hooks/useFetch";
+import Loader from "./Loader";
 
 const QuoteBox = () => {
   const tiltRef = useRef(null);
-
+  const [previousId, setPreviousId] = useState(null);
+  const dataFetchedRef = useRef(false);
+  const { data, error, loading } = useFetch(
+    "https://api.quotable.io/quotes/random"
+  );
+  useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+    if (data && data.length > 0) {
+      setPreviousId(data[0]["_id"]); // Save "_id" on loading
+    }
+  }, [data]);
   useEffect(() => {
     // Initialize vanilla-tilt on the element
     VanillaTilt.init(tiltRef.current, {
@@ -20,28 +33,37 @@ const QuoteBox = () => {
     };
   }, []);
 
+  const doubleQuote = '"';
   return (
     <div id="quote-box" ref={tiltRef}>
-      <div id="text">
-        {
-          '"La vida es lo que pasa mientras estás ocupado haciendo otros planes."'
-        }
-      </div>
-      <div id="author">- John Lennon</div>
-      <div className="buttons">
-        <a
-          className="button button-icon"
-          id="tweet-quote"
-          href="https://twitter.com/intent/tweet?text=La%20vida%20es%20lo%20que%20pasa%20mientras%20est%C3%A1s%20ocupado%20haciendo%20otros%20planes.%20-%20John%20Lennon"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={twitterSVG} alt="Twitter" />
-        </a>
-        <button className="button" id="new-quote">
-          New quote
-        </button>
-      </div>
+      {data ? (
+        <>
+          <div id="text">
+            {doubleQuote}
+            {data[0].content}
+            {doubleQuote}
+          </div>
+          <div id="author">- {data[0].author}</div>
+          <div className="buttons">
+            <a
+              className="button button-icon"
+              id="tweet-quote"
+              href={`https://twitter.com/intent/tweet?text="${encodeURIComponent(
+                data[0].content + '"' + " - " + data[0].author
+              )}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={twitterSVG} alt="Twitter" />
+            </a>
+            <button className="button" id="new-quote">
+              New quote
+            </button>
+          </div>
+        </>
+      ) : (
+        loading && <Loader />
+      )}
     </div>
   );
 };
